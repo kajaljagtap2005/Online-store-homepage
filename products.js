bbf8221d0281552b1e3b58b0c3f6a6a446b02b00
kajaljagtap2005 <kajaljagtapofficial@gmail.com>
2026-03-25 19:43:57 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,4 @@
+const products = [
+    { id: 1, name: "Neural Bass Headphones", category: "Electronics", price: 299, rating: 5, img: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=800" },
+    { id: 2, name: "Minimalist Desk Lamp", category: "Home", price: 85, rating: 4, img: "https://images.unsplash.com/photo-1534073828943-f801091bb18c?w=800" },
+    
